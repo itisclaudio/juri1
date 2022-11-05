dca3c075b3d64dbc63c6b7eebbe09338ed969103
Claudio Roman <itisclaudio@gmail.com>
2022-11-05 14:49:46 -0400

    db vars in settings

--- project/settings_dev.py
+++ project/settings_dev.py
@@ -1,1 +1,15 @@
+import os
+
 print('Development Settings')
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+    }
+}
